c620dc58befe8d114ea6e7ef33aa68cccf075d26
victorbrigido <110338761+victorbrigido@users.noreply.github.com>
2023-10-07 09:35:53 -0300

    Update firebase_config.js

--- src/Config/firebase_config.js
+++ src/Config/firebase_config.js
@@ -4,17 +4,17 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getFirestore, collection, addDoc, doc, deleteDoc, getDocs, serverTimestamp } from "firebase/firestore";
 
-//Coloque as suas informações e chaves aqui
+//Coloque as suas informações e chaves aqui *****
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1OaezIfvtv6KxhPh_BUGXanv5-WfocxM",
-  authDomain: "log-auth-c31cd.firebaseapp.com",
-  databaseURL: "https://log-auth-c31cd-default-rtdb.firebaseio.com",
-  projectId: "log-auth-c31cd",
-  storageBucket: "log-auth-c31cd.appspot.com",
-  messagingSenderId: "199338007194",
-  appId: "1:199338007194:web:787361298fe1cd848e1d23",
-  measurementId: "G-WBJNPKT36E",
+  apiKey: "*****************************",
+  authDomain: "**************************",
+  databaseURL: "*************************",
+  projectId: "*************************",
+  storageBucket: "*************************",
+  messagingSenderId: "*************************",
+  appId: "*************************",
+  measurementId: "*************************",
 };
 
 
